4faa04bef29afc966eb92e1bce5635f25bc4dff0
Felix Pacheco <devfex.19@gmail.com>
2025-04-15 16:47:14 -0400

    chore: Update import of adapters

--- host-app/src/AppContent.tsx
+++ host-app/src/AppContent.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import ArcaneCharactersAdapter from './adapters/ArcaneCharactersAdapter';
-import TlouCharactersAdapter from './adapters/TlouCharactersAdapter';
+import { ArcaneCharactersAdapter } from './adapters/ArcaneCharactersAdapter';
+import { TlouCharactersAdapter } from './adapters/TlouCharactersAdapter';
 import { Container, Header, Selector } from './App.styles';
 import { ArcaneCharacter } from './entities/ArcaneCharacter';
 import { TlouCharacter } from './entities/TlouCharacter';
