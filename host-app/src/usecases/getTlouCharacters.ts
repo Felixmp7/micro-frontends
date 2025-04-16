81448ab95cf41d0132698378d47781e793c5738d
Felix Pacheco <devfex.19@gmail.com>
2025-04-16 19:57:13 -0400

    refactor: remove unused assets and code for cleaner project structure

--- host-app/src/usecases/getTlouCharacters.ts
+++
@@ -1,8 +0,0 @@
-import { TlouCharacter } from '../entities/TlouCharacter';
-
-// Simulación de fetch. En producción, reemplazar por llamada real a API.
-export const getTlouCharacters = async (): Promise<TlouCharacter[]> => [
-  { name: 'Joel', image: 'https://static.wikia.nocookie.net/thelastofus/images/0/09/Joel_TLOU_Part_I_Render.png' },
-  { name: 'Ellie', image: 'https://static.wikia.nocookie.net/thelastofus/images/9/9a/Ellie_TLOU_Part_I_Render.png' },
-  { name: 'Abby', image: 'https://static.wikia.nocookie.net/thelastofus/images/7/7b/Abby_TLOU_Part_II_Render.png' },
-];
