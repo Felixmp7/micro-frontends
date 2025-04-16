48b9ab9806a0049c7c8152c854608e302252b1d3
Felix Pacheco <devfex.19@gmail.com>
2025-04-16 19:59:52 -0400

    feat: update Arcane and TLOU character adapters to use ListOfCharacters component

--- host-app/src/adapters/ArcaneCharactersAdapter.tsx
+++ host-app/src/adapters/ArcaneCharactersAdapter.tsx
@@ -1,8 +1,17 @@
-import { ArcaneCharacters } from 'arcaneMf/ArcaneCharacters';
+import { CharactersList } from 'arcaneMf/CharactersList';
 import type { SeriesCharacter } from 'shared-entities';
 
 interface Props {
+  language: 'en' | 'es';
   characters: SeriesCharacter[];
+  isLoading: boolean;
+  handleLoadCharacters: VoidFunction;
 }
 
-export const ArcaneCharactersAdapter = ({ characters }: Props) => <ArcaneCharacters characters={characters} />;
+export const ArcaneCharactersAdapter = ({ characters, isLoading, handleLoadCharacters, language }: Props) => <CharactersList
+    language={language}
+    buttonText={language === 'en' ? 'Load Arcane Characters' : 'Cargar personajes de Arcane'}
+    characters={characters}
+    isLoading={isLoading}
+    onClick={handleLoadCharacters}
+/>
